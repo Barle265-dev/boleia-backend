f38deb17212f0cf4dab8b7c73ec4c2d5b079f80f
Joel Oliveira <joelbarle2001@gmail.com>
2026-06-07 10:13:50 -0100

    import routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from "fastify";
-import { freightRoutes } from "controllers/freights/freight.routes";
-import { permissionRoutes } from "routes/permission.routes";
-import { rideRoutes } from "controllers/rides/ride.routes";
-import { uploadRoutes } from "routes/upload.routes";
-import { userRoutes } from "controllers/users/user.routes";
-import { notificationRoutes } from "controllers/notifications/notification.routes";
-import { vehicleRoutes } from "controllers/vehicles/vehicle.routes";
-import { messageRoutes } from "controllers/messages/message.routes";
+import { freightRoutes } from "../controllers/freights/freight.routes";
+import { permissionRoutes } from "./permission.routes";
+import { rideRoutes } from "../controllers/rides/ride.routes";
+import { uploadRoutes } from "./upload.routes";
+import { userRoutes } from "../controllers/users/user.routes";
+import { notificationRoutes } from "../controllers/notifications/notification.routes";
+import { vehicleRoutes } from "../controllers/vehicles/vehicle.routes";
+import { messageRoutes } from "../controllers/messages/message.routes";
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(userRoutes);
